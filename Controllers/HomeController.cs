ea39a8b82e9d70cc3aaa70c78b583f41fccbfc67
Deepak Kumar Mishra <demishra@microsoft.com>
2020-04-15 02:06:05 +0530

    model in dotnet core with new classes in model and interfaces to introduce injection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EmpManagement.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
-        public JsonResult Index()
+        // GET: /<controller>/
+        public IActionResult Index()
         {
-            return this.Json(new { id=1, name="Deepak"});
+            return View();
         }
     }
 }
---
+++ Models/MockEmployeeRepocitory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpManagement.Models
+{
+    public class MockEmployeeRepocitory : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepocitory()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name ="Mary", Department = "HR", Email = "x@y.com"},
+            new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "x@y.com" },
+                new Employee(){Id = 1, Name ="Mary", Department = "HR", Email = "x@y.com"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
