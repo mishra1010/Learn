42e18c6f1f2d5f4075d596de1c96590886f5d254
Frenemy2020 <demishra@microsoft.com>
2020-05-16 20:13:13 +0530

    added validation attributes in employee.cs and made necessary changes in controller and create.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -45,10 +45,14 @@ namespace EmpManagement.Controllers
 
 
         [HttpPost]
-        public RedirectToActionResult Create(Employee employee)
+        public IActionResult Create(Employee employee)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            return RedirectToAction("details", new { id = newEmployee.Id });
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
         }
     }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace EmpManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
         public Dept Department { get; set; }
     }
