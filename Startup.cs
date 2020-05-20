f171d0a25f124a29b891acdaefee5b06c9954788
Frenemy2020 <demishra@microsoft.com>
2020-05-20 07:48:21 +0530

    Entityframeworkcore - added dbcontext class created dbset for employee entity

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace EmpManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddMvc();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepocitory>();
         }
