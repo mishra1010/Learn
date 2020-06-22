2f9eb94210e5c9cc44a0a79dd67bb114342c3839
Frenemy2020 <demishra@microsoft.com>
2020-06-22 08:12:48 +0530

    reExecute middleware preserves the error

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,7 @@ namespace EmpManagement
             }
             else
             {
-                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
             }
         
             app.UseStaticFiles();
